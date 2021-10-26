4982447ec84e94907c0c6f1f14c7b32462256fde
jo <ljonas@riseup.net>
2021-10-26 12:26:21 +0200

    WIP

--- howler/howler.py
+++ howler/howler.py
@@ -2,24 +2,29 @@
 
 from subprocess import Popen, run
 from time import sleep
-from threading import thread
+from threading import Thread
+from queue import Queue
 
 
-def icecast():
-    with Popen(["icecast", "-c", "./icecast.xml"]) as icecast_process:
-        yield
-        icecast_process.kill()
+class Howler(Thread):
+    queue: Queue
 
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
 
-def stream_sample():
-    run(
-        (
-            *("ffmpeg", "-hide_banner", "-nostats"),
-            "-re",
-            *("-i", "sample.ogg"),
-            *("-f", "ogg"),
-            *("-content_type", "audio/ogg"),
-            "icecast://source:hackme@localhost:8000/howler.ogg",
-        ),
-        check=True,
-    )
+    def run(self):
+        with Popen(["icecast", "-c", "./icecast.xml"]) as icecast_process:
+            for item in self.queue:
+                run(
+                    (
+                        *("ffmpeg", "-hide_banner", "-nostats"),
+                        "-re",
+                        *("-i", "sample.ogg"),
+                        *("-f", "ogg"),
+                        *("-content_type", "audio/ogg"),
+                        "icecast://source:hackme@localhost:8000/howler.ogg",
+                    ),
+                    check=True,
+                )
+                self.queue.task_done()
